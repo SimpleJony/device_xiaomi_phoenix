12764d5a65fbfb74b563a599437deb516141985d
Volodymyr Zhdanov <wight554@gmail.com>
2021-08-06 20:48:55 +0530

    phoenix: Build vendor.qti.hardware.display.mapper@1.1 interface
    * needed by /vendor/lib64/sensors.ssc.so
    
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- device.mk
+++ device.mk
@@ -221,6 +221,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer@1.0.vendor \
     vendor.qti.hardware.display.composer@2.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0 \
     vendor.qti.hardware.display.mapper@4.0.vendor
