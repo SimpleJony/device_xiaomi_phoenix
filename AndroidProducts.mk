0a216f4f2e394908b2f7a68f0d66d699e4286880
chiru2000 <chiranthangadi123@gmail.com>
2021-12-01 17:02:27 +0530

    Setup PE
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_phoenix.mk
+    $(LOCAL_DIR)/aosp_phoenix.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_phoenix-user \
+    aosp_phoenix-userdebug \
+    aosp_phoenix-eng
--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Custom stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_phoenix
+PRODUCT_NAME := aosp_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
