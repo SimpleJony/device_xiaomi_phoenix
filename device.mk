5f427ddc13c8b50f5251aa479cda8f2a96d68960
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-01-19 19:11:34 +0530

    phoenix: rootdir: Remove init.qcom.sdio.sh
    Change-Id: I4a1be05d7b4e1742bb799aa615158f875896b1e4

--- device.mk
+++ device.mk
@@ -478,7 +478,6 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
