4e6f9ec5237910f73b2cb95aa43cfd66a168b94a
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-03-07 11:10:09 +0000

    phoenix: Enabled/Added Support For Google Dialer Call Recording

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Call recording
+PRODUCT_PACKAGES += \
+    com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
