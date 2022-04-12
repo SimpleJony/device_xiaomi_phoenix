6519f748217e1ca3e76fc46803d8b3c06a9e9ac3
SharmagRit <sharmagrit04@gmail.com>
2022-04-12 10:28:12 +0000

    phoenix: ProjectArcana bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_phoenix.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_phoenix-user \
+    aosp_phoenix-userdebug \
+    aosp_phoenix-eng
+
