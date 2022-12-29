c09bb582af0ee651984813a3bf0dbf0e36a988c0
SimpleJony <jonylovesmile@outlook.com>
2022-12-29 15:24:40 +0800

    phoenix: Build with trb clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,10 @@
 echo "Cloning kernel tree..."
-git clone https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1 -b pure-phoenix --depth=1 kernel/xiaomi/phoenix
+proxychains git clone https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1 -b pure-phoenix --depth=1 kernel/xiaomi/phoenix
 echo "Cloning vendor tree..."
-git clone https://github.com/SharmagRit/android_vendor_xiaomi_phoenix -b 13 --depth=1 vendor/xiaomi/phoenix
+proxychains git clone https://github.com/SharmagRit/android_vendor_xiaomi_phoenix -b 13 --depth=1 vendor/xiaomi/phoenix
 echo "Cloning hardware/xiaomi..."
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+proxychains git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 echo "Cloning ifaa service..."
-git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras device/xiaomi/extras
+proxychains git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras device/xiaomi/extras
+echo "Cloning trb clang..."
+proxychains git clone https://gitlab.com/varunhardgamer/trb_clang --depth=1 prebuilts/clang/host/linux-x86/trb_clang
