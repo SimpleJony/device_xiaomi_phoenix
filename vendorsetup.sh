0ae544d7721494a568ea138dd7850311b8ca11ed
SimpleJony <jonylovesmile@outlook.com>
2022-12-30 19:52:27 +0800

    phoenix: Build with proton clang
    Signed-off-by: SimpleJony <jonylovesmile@outlook.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo "Cloning kernel tree..."
 git clone https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1 -b pure-phoenix --depth=1 kernel/xiaomi/phoenix
 echo "Cloning vendor tree..."
-git clone https://github.com/SharmagRit/android_vendor_xiaomi_phoenix -b 13 --depth=1 vendor/xiaomi/phoenix
-echo "Cloning hardware/xiaomi..."
-git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi -b arrow-13.0 hardware/xiaomi
+git clone https://github.com/SimpleJony/vendor_xiaomi_phoenix --depth=1 vendor/xiaomi/phoenix
+echo "Cloning proton clang..."
+git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-proton
