4dced531d79077a0bc50ecf8568c439298537bab
SimpleJony <jonylovesmile@outlook.com>
2022-12-30 20:15:10 +0800

    phoenix: Clone ifaa service

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
 echo "Cloning kernel tree..."
-git clone https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1 -b pure-phoenix --depth=1 kernel/xiaomi/phoenix
+sudo git clone https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1 -b pure-phoenix --depth=1 kernel/xiaomi/phoenix
 echo "Cloning vendor tree..."
-git clone https://github.com/SimpleJony/vendor_xiaomi_phoenix --depth=1 vendor/xiaomi/phoenix
+sudo git clone https://github.com/SimpleJony/vendor_xiaomi_phoenix --depth=1 vendor/xiaomi/phoenix
 echo "Cloning proton clang..."
-git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+sudo git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+echo "Cloning ifaa service..."
+sudo git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras device/xiaomi/extras
