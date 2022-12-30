df198b17dcbd563551fd40f33a204b82dd62c642
SimpleJony <jonylovesmile@outlook.com>
2022-12-30 09:37:39 +0800

    phoenix: Nuke proxychains

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 echo "Cloning kernel tree..."
-proxychains git clone https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1 -b pure-phoenix --depth=1 kernel/xiaomi/phoenix
+git clone https://github.com/Pzqqt/android_kernel_xiaomi_sm6150-1 -b pure-phoenix --depth=1 kernel/xiaomi/phoenix
 echo "Cloning vendor tree..."
-proxychains git clone https://github.com/SharmagRit/android_vendor_xiaomi_phoenix -b 13 --depth=1 vendor/xiaomi/phoenix
+git clone https://github.com/SharmagRit/android_vendor_xiaomi_phoenix -b 13 --depth=1 vendor/xiaomi/phoenix
 echo "Cloning hardware/xiaomi..."
-proxychains git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 echo "Cloning ifaa service..."
-proxychains git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras device/xiaomi/extras
+git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras device/xiaomi/extras
