561a069bb74f218d694fb675e1b59ed9f0972970
SimpleJony <jonylovesmile@outlook.com>
2023-01-16 19:55:31 +0800

    phoenix: Use common_full_phone.mk
    Signed-off-by: SimpleJony <jonylovesmile@outlook.com>

--- derp_phoenix.mk
+++ derp_phoenix.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_phoenix
