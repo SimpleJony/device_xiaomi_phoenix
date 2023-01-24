ea6a8b6cacfb712272f4c0cffca3dfc08f17d917
SimpleJony <jonylovesmile@outlook.com>
2023-01-24 22:22:27 +0800

    [TEST]phoenix: Miui Camera: Adapt for phoenix

--- device.mk
+++ device.mk
@@ -118,17 +118,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    libdng_sdk.vendor \
-    libgui_vendor \
-    libxml2 \
-    GCam \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    android.hidl.memory.block@1.0 \
-    android.hidl.memory.block@1.0.vendor \
-    libstdc++.vendor
+    android.hardware.camera.provider@2.4-service_64
 
 # Crypto
 PRODUCT_PACKAGES += \
@@ -385,6 +376,19 @@ PRODUCT_PACKAGES += \
 
 # MiuiCamera
 $(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
+
+# Miui Camera Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/default-permissions/miuicamera-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/miuicamera-permissions.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml \
+    $(LOCAL_PATH)/configs/permissions/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    $(LOCAL_PATH)/configs/permissions/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.front.xml \
+    $(LOCAL_PATH)/configs/permissions/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.full.xml \
+    $(LOCAL_PATH)/configs/permissions/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.camera.raw.xml \
+    $(LOCAL_PATH)/configs/device_features/phoenixin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/device_features/phoenixin.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml
 
 # NeuralNetworks
 PRODUCT_PACKAGES += \
