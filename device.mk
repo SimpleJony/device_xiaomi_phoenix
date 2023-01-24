8c4e69f3229e662f2e07a063b415041eead539a0
SimpleJony <jonylovesmile@outlook.com>
2023-01-24 23:35:59 +0800

    phoenix: Build android.hidl.memory.block@1.0
    *Needed by vendor.xiaomi.hardware.misys@3.0-service
    
    Signed-off-by: SimpleJony <jonylovesmile@outlook.com>

--- device.mk
+++ device.mk
@@ -119,7 +119,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory.block@1.0.vendor
 
 # Crypto
 PRODUCT_PACKAGES += \
