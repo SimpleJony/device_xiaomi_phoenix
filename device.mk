bccc5edc898d8b57f0249188980575933a3c1ec1
Jony <108582874+SimpleJony@users.noreply.github.com>
2023-01-24 16:23:24 +0800

    phoenix: Copy powerhint.json

--- device.mk
+++ device.mk
@@ -435,6 +435,9 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
