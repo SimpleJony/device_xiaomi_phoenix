1073dd1d54518f0d391276c2acb943a7790eca3b
SimpleJony <jonylovesmile@outlook.com>
2023-01-30 21:14:26 +0800

    phoenix: Build vendor.qti.hardware.camera.device@3.5.vendor

--- device.mk
+++ device.mk
@@ -121,12 +121,12 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    android.hardware.camera.provider@2.7 \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
     GCam \
     vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.device@3.5.vendor \
     android.hidl.memory.block@1.0 \
     android.hidl.memory.block@1.0.vendor \
     libstdc++.vendor
