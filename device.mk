4d4bffe58c7e7f32d39ca0d3092f0415a1978bfd
SimpleJony <jonylovesmile@outlook.com>
2023-01-30 19:56:34 +0800

    phoenix: Use android.hardware.camera.provider@2.7 *Looks 2.6 is useless

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    android.hardware.camera.provider@2.6 \
+    android.hardware.camera.provider@2.7 \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
