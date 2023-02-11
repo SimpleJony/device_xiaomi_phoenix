81ea3c57c3fa4711f68d815ef765e6a312a42db8
SimpleJony <jonylovesmile@outlook.com>
2023-02-11 20:02:07 +0800

    phoenix: Update build fingerprint to February 2023

--- derp_phoenix.mk
+++ derp_phoenix.mk
@@ -25,8 +25,8 @@ TARGET_USES_BLUR := true
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="cheetah-user 13 TQ1A.230105.001.A2 9325679 release-keys"
+	PRIVATE_BUILD_DESC="cheetah-user 13 TQ1A.230205.002 9471150 release-keys"
 
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ1A.230105.001.A2/9325679:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ1A.230205.002/9471150:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
