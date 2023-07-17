4e39d451e8399c1ee200f6a4cead68e66a7bf2b7
SimpleJony <jonylovesmile@outlook.com>
2023-07-17 23:25:44 +0800

    phoenix: Remove permission for GCam
    * We don't inbuilt gcam now

--- device.mk
+++ device.mk
@@ -428,11 +428,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
-
-# Gcam
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/permission/privapp-permissions-gcam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-gcam.xml
-
+    
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.phoenix-libperfmgr
