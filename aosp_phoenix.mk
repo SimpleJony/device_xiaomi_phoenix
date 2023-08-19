1aff29d677c888c0ce97f8fb0e6bebe7568565fe
SimpleJony <jonylovesmile@outlook.com>
2023-08-19 15:27:30 +0800

    phoenix:Update fingerprints to Cheetah August 2023

--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -30,8 +30,8 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 13 TP1A.221005.002/9012097 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230805.001 10316531 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230805.001/10316531:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
